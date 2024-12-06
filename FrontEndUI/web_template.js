663ff0725be731f3a938756116e61fe4a2d706d6
vishal chauhan <vishal chauhan@CHAUHANGX-LAP>
2024-12-06 22:19:46 +0530

    Ui Changes

--- FrontEndUI/web_template.js
+++ FrontEndUI/web_template.js
@@ -1,4 +1,13 @@
-function openCustomizationForm(url,id) {
+window.onload = function () {
+    const username ="Hi "+ localStorage.getItem('email');
+    if (username) {
+        document.getElementById('username').textContent = username;
+    } else {
+        document.getElementById('username').textContent = 'Guest';
+    }
+};
+
+function openCustomizationForm(url, id) {
 	console.log("id received is :",id);
 	localStorage.setItem("template_Id",id);
   // Open the specified URL in the same tab
